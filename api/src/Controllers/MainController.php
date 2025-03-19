5ca3bde1c865f2762414d1eb1e8e6dfe2fd7d263
Denis Maksimov <54079186+porabote@users.noreply.github.com>
2025-03-19 18:34:51 +0300

    Update MainController.php

--- api/src/Controllers/MainController.php
+++ api/src/Controllers/MainController.php
@@ -21,37 +21,6 @@ class MainController
 
         $phone = $this->request->input->get('Phone');
 
-
-        $data = $this->request->all();
-
-        if (empty($data['Сколько_Вам_лет?'])) {
-            exit();
-        }
-        if (empty($data['Phone'])) {
-            exit();
-        }
-
-
-        if (!empty($data["Какие_активности_(акции)_вы_бы_хотели,_что_бы_появились_в_клубах?"])) {
-            $this->isIssetCyrillic($data['Какие_активности_(акции)_вы_бы_хотели,_что_бы_появились_в_клубах?']);
-        }
-
-        if (!empty($data["Что_нам_нужно_продолжать_делать_в_наших_Клубах?"])) {
-            $this->isIssetCyrillic($data['Что_нам_нужно_продолжать_делать_в_наших_Клубах?']);
-        }
-
-        if (!empty($data["Что_нам_нужно_продолжать_делать_в_наших_Клубах?_2"])) {
-            $this->isIssetCyrillic($data['Что_нам_нужно_продолжать_делать_в_наших_Клубах?_2']);
-        }
-
-        if (!empty($data["Что_нам_нужно_прекратить_делать_в_наших_Клубах?"])) {
-            $this->isIssetCyrillic($data['Что_нам_нужно_прекратить_делать_в_наших_Клубах?']);
-        }
-
-        if (!empty($data["Что_нам_нужно_прекратить_делать_в_наших_Клубах?_2"])) {
-            $this->isIssetCyrillic($data['Что_нам_нужно_прекратить_делать_в_наших_Клубах?_2']);
-        }
-
         $data = [
             'api' => 'ec3459df2701beb8334357d9ba7f283d',
             'type' => 1929,
@@ -71,19 +40,19 @@ class MainController
             ->setData($data)
             ->post();
 
-        $data = array_merge($this->request->all(), [
-            'handler' => 'opros2025',
-            'action' => 'writeToGoogleTable',
-            'type' => 1,
-        ]);
+        // $data = array_merge($this->request->all(), [
+        //     'handler' => 'opros2025',
+        //     'action' => 'writeToGoogleTable',
+        //     'type' => 1,
+        // ]);
 
 
-        $resp = ApiClient::init($this->configs)
-            ->setHost('https://cp.passmen.club/api')
-            ->setUrl('/landings/action/handleData')
-            ->setResponseFormat('HTTP')
-            ->setData($data)
-            ->post();
+        // $resp = ApiClient::init($this->configs)
+        //     ->setHost('https://cp.passmen.club/api')
+        //     ->setUrl('/landings/action/handleData')
+        //     ->setResponseFormat('HTTP')
+        //     ->setData($data)
+        //     ->post();
 
         return $this->response->json([
             'result' => 'OK', // TODO error
